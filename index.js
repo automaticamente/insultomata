f2ab8075eee3acd5af210149dd09a402854d4d1f
Berto Yáñez <berto@ber.to>
2018-03-19 16:21:03 +0100

    avances 123

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const TG = require('treegrammar');
+const color = require('tinycolor2');
+const stitchit = require('./lib/stitchit');
+
+const { sample } = require('lodash');
 
 const insultos = require('./insultos.json');
 
@@ -6,21 +12,42 @@ const rules = {
   '<start>': ['<template>'],
   '<template>': [
     'Es un <insulto> e un <insulto>, vai <accion> <a_lugar>.',
-    'Ai <insulto> <modificador>, vai <accion>!',
+    'Mira que es <insulto> <modificador>, vai <accion>!',
     'Me cago <familiar> que te <orixe>, <insulto> <modificador>.',
-    'Vai <accion> <a_lugar>, <insulto> da <sexual_feminino>.'
+    'Vai <accion> <a_lugar>, <insulto> da <sexual_feminino>.',
+    'Vai <accion> <a_lugar>, <insulto> do <sexual_masculino>.'
   ],
   '<insulto>': insultos.map(i => i.toLowerCase()),
   '<familiar>': ['na nai', 'na <sexual_feminino>'],
-  '<sexual_feminino>': ['cona', 'caralla', 'berberecha', 'perrecha'],
+  '<sexual_feminino>': ['cona', 'berberecha', 'perrecha'],
+  '<sexual_masculino>': ['carallo', 'pirola'],
   '<orixe>': ['botou', 'cagou', 'chimpou'],
   '<modificador>': ['de merda', 'do carallo'],
-  '<accion>': ['mexar', 'cagar', 'tocar a <sexual_feminino>'],
-  '<a_lugar>': ['ao pe dunha cola', 'ao campo', 'á corte']
+  '<accion>': [
+    'mexar',
+    'cagar',
+    'tocar a <sexual_feminino>',
+    'rañar na <sexual_feminino>',
+    'tocar o <sexual_masculino>'
+  ],
+  '<a_lugar>': ['ao pe dunha cola', 'ao campo', 'á corte', 'lonxe de aquí']
 };
 
 const generator = new TG(rules);
 
-for (let i = 0; i < 15; i++) {
-  console.log(generator.generate());
+module.exports.bot = function bot() {
+  const i = generator.generate().toUpperCase();
+  const background = color.random().toHexString();
+
+  stitchit({
+    text: i,
+    font: path.join(__dirname, `fonts/stitch${sample([1, 2, 3])}.ttf`),
+    fontSize: '60',
+    size: '600x600',
+    background
+  }).then(buffer => fs.writeFileSync('./out.png', buffer));
+};
+
+if (require.main === module) {
+  module.exports.bot();
 }
